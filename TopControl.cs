3910a741e5f9870883bdf9a9cf5225918257be1f
Eren M <47501497+kristajic@users.noreply.github.com>
2019-12-10 18:18:12 +0300

    Add files via upload

---
+++ TopControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopControl : MonoBehaviour
+{
+    Rigidbody physc;
+    public int speed;
+    public int Toplanacakobjesayisi;
+    public Text scoreText;
+    public Text complatedText;
+    int point = 0;
+    void Start()
+    {
+        physc = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+
+        Vector3 vector = new Vector3(yatay,0,dikey);
+
+        physc.AddForce(vector*speed);
+       
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "engel")
+        {
+            other.gameObject.SetActive(false);
+            point++;
+
+            scoreText.text = "Score: " + point;
+
+            if (point == Toplanacakobjesayisi)
+            {
+                complatedText.text = "Complated";
+            }
+
+        }
+       
+
+    }
+}
